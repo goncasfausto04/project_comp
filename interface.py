ef96b33cfc863aa7e3d4fcd01b00046d17fa74d5
goncasfausto04 <20231721@novaims.unl.pt>
2024-10-25 15:20:03 +0100

    coiso

---
+++ interface.py
@@ -0,0 +1,34 @@
+import pygame
+from utils import * # no need to import pygame because the import is in utils
+from config import * # importing colors and the like
+
+def interface():
+
+    # initiating pygame
+    pygame.init()
+    # creating the screen at the set resolution
+    screen = pygame.display.set_mode(resolution)
+
+    pass
+
+
+# Under construction screen
+def under_construction():
+
+    # creating the screen at 720x720 pixels
+    screen = pygame.display.set_mode(resolution)
+
+    pass
+
+def credits_():
+    screen = pygame.display.set_mode(resolution)
+
+    pass
+
+
+def rules_():
+    print("Displaying rules...")
+
+
+def wilderness_explorer():
+    print("Wilderness Explorer Game Starting...")
